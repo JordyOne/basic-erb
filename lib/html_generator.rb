2b3cc9f1f5b0965ef21eaf6f2f1be4264fa7a01d
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-25 12:01:34 -0600

    completed erb exercise

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -1,5 +1,45 @@
 require "erb_generator"
 
 class HTMLGenerator < ERBGenerator
- 
+  def initialize(text=nil)
+    @wrapper = text
+  end
+
+
+  def section(text)
+    template = <<-TEMPLATE
+<section><%= section_text %></section>
+    TEMPLATE
+    options_hash= {:locals => {:section_text => text},
+                   :layout => @wrapper}
+    erb(template, options_hash)
+  end
+
+  def unordered_list(text)
+    template = <<-TEMPLATE
+<ul>
+<% list_each.each do |item|%>
+<% item %>
+<li><%= item %></li>
+<%end%>
+</ul>
+    TEMPLATE
+    options_hash = {:locals => {:list_each => text},
+                    :layout => @wrapper}
+    erb(template, options_hash)
+  end
+
+  def button(text, class_h = {})
+    if class_h != {}
+      template = "<button class='<%=class_hash[:class]%>'><%=button_text%></button>"
+    else
+      template = "<button><%=button_text%></button>"
+    end
+    options_hash = {:locals => {:button_text => text, :class_hash => class_h},
+                    :layout => @wrapper}
+    erb(template, options_hash)
+
+  end
 end
+
+
